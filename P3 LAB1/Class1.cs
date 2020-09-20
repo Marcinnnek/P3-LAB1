12d87ef80c023ee9b826dd4061ee40146d92e255
Marcin <Marcin@DESKTOP-P5HQ09B>
2020-09-20 11:17:28 +0200

    COMMIT 2

---
+++ P3 LAB1/Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace P3_LAB1
+{
+    public class Class1
+    {
+        public static void Fun1()
+        {
+            Console.WriteLine("klasa1");
+        }
+    }
+}
--- P3 LAB1/Program.cs
+++ P3 LAB1/Program.cs
@@ -7,6 +7,9 @@ namespace P3_LAB1
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Class1 obj1 = new Class1;
+            obj1.Fun1();
+            
         }
     }
 }
